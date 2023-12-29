ef9f27219cf6f73d05c3164a630deecaa27dd082
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-29 17:51:21 +0330

    install composer

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ exit
 
 echo "Your public key is authorized for root and git users"
 
-#install nginx, mysql, php, and redis
+#install nginx, mysql, php, redis and composer
 echo "Installing nginx, mysql, and php"
 
 apt update
@@ -123,6 +123,20 @@ pecl install redis
 apt install php-redis
 sed -i '' 's/.*extension=redis.so.*/extension=redis.so/' /etc/php/$PHP_VERSION/cli/conf.d/20-redis.ini
 service php$PHP_VERSION-fpm reload
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer --quiet
+rm composer-setup.php
 
 # set up git
 cd /var/www/html
