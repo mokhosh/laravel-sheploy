c4be6fc88d2cc325e802a01e46359b96fa3cd37b
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-29 18:20:49 +0330

    remove unnecessary \n

--- sheploy.sh
+++ sheploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # get the client public key
-echo "Loading your public SSH key...\n"
+echo "Loading your public SSH key..."
 CLIENT_KEY=$(<~/.ssh/id_rsa.pub)
-echo "Copy this so you can use it while setting up your server\n"
+echo "Copy this so you can use it while setting up your server"
 echo $CLIENT_KEY
 
 # get the server ip from user
-echo "Enter your remote server's IP adress:\n"
+echo "Enter your remote server's IP adress:"
 read IP
 
 # download the necessary script to the server
-echo "Downloading the scripts on your server ~/laravel-sheploy...\n"
+echo "Downloading the scripts on your server ~/laravel-sheploy..."
 ssh root@$IP << EOF
 apt install git -y
 cd ~
