20b17ce5993fd405e099fc40759a2e56377040e6
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-29 16:20:31 +0330

    add public key to root

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 # get the public ssh key from user
 echo "Enter your public ssh key:\n"
 read CLIENT_KEY
+
+# put the public key in root authorized keys
+cd ~/.ssh
+echo $CLIENT_KEY >> authorized_keys
