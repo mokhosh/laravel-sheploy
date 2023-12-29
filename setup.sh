0da0a1ba87cb9aa2e8128ef286b22c4d9724ec38
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-29 23:42:13 +0330

    add php intl extension

--- setup.sh
+++ setup.sh
@@ -72,7 +72,7 @@ add-apt-repository ppa:ondrej/php -y
 apt update
 apt install php$PHP_VERSION-fpm php$PHP_VERSION-common php$PHP_VERSION-mysql \
     php$PHP_VERSION-xml php$PHP_VERSION-xmlrpc php$PHP_VERSION-curl \
-    php$PHP_VERSION-gd php$PHP_VERSION-imagick php$PHP_VERSION-cli \
+    php$PHP_VERSION-gd php$PHP_VERSION-imagick php$PHP_VERSION-cli php$PHP_VERSION-intl \
     php$PHP_VERSION-dev php$PHP_VERSION-imap php$PHP_VERSION-mbstring \
     php$PHP_VERSION-opcache php$PHP_VERSION-soap php$PHP_VERSION-zip unzip -y
 
