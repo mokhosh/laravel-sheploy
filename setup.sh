81e8b9b1b5ee8ff4c0bfd7c7216751e728278304
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-29 16:33:19 +0330

    install php

--- setup.sh
+++ setup.sh
@@ -43,3 +43,10 @@ mysql \
   --user="root" \
   --password="$PASSWORD" \
   --execute="FLUSH PRIVILEGES;"
+
+apt install software-properties-common
+add-apt-repository ppa:ondrej/php
+apt update
+apt install php8.3-fpm php8.3-common php8.3-mysql php8.3-xml php8.3-xmlrpc \
+    php8.3-curl php8.3-gd php8.3-imagick php8.3-cli php8.3-dev php8.3-imap \
+    php8.3-mbstring php8.3-opcache php8.3-soap php8.3-zip unzip -y
