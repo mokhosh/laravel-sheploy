b3ddb1d52ffc477f093b5adca356f044bc5c37c4
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-29 16:20:56 +0330

    put the public key in git user

--- setup.sh
+++ setup.sh
@@ -11,3 +11,11 @@ echo $CLIENT_KEY >> authorized_keys
 #create git user
 adduser git
 usermod -aG sudo git
+
+# put the public key in git authorized keys
+su git
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+echo $CLIENT_KEY >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+exit
